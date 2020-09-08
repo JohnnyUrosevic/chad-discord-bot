94cbf2b531b3826b4ad08c1e2d93a528a5a0f75b
Johnny Urosevic <themagicalcake@gmail.com>
2020-09-08 03:04:39 -0700

    Download all attachments and linked images

--- bot.py
+++ bot.py
@@ -1,7 +1,27 @@
 import discord
+import aiofiles
+from os import path
+from aiohttp import ClientSession
 from config import API_TOKEN
 
 client = discord.Client()
+
+# Add numbers to duplicately named files to save them to different files
+def get_filename(name):
+    filename = f'images/{name}' + "0"
+    count = 1
+    while path.isfile(filename):
+        filename = filename[:-1] + str(count)
+        count += 1
+    return filename
+
+# Downloads a request from an embed
+async def save_embed(embed):
+    async with ClientSession() as session:
+        async with session.get(embed.url) as response:
+            filename = get_filename(embed.url.split("/")[-1])
+            async with aiofiles.open(filename, 'wb') as file:
+                await file.write(await response.read())
 
 @client.event
 async def on_ready():
@@ -12,7 +32,13 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    # Only images and videos have a height
+    for attachment in message.attachments:
+        if attachment.height is not None:
+            await attachment.save(get_filename(attachment.filename))
+
+    for embed in message.embeds:
+        if embed.type == "image":
+            await save_embed(embed)
 
 client.run(API_TOKEN)
