2141536365df2b4bfe146053846efce46d0c9dcd
Johnny Urosevic <themagicalcake@gmail.com>
2020-09-08 01:58:25 -0700

    Add token secretly and gitignore

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
 import discord
+from config import API_TOKEN
 
 client = discord.Client()
-
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
 
 @client.event
 async def on_message(message):
@@ -16,4 +15,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('your token here')
+client.run(API_TOKEN)
